cf7ea7398d89755ac10c8d55c23f73a4473f883c
Emma Poliakova <2389098p@student.gla.ac.uk>
2021-07-24 18:20:30 +0200

    working npm

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import uniqueRandomArray from 'unique-random-array';
+import lodash from 'lodash';
+//var output = lodash.without([1, 2, 3], 1);
+//console.log(output);
+// const dogs = [
+//   { 'name': 'snickers', 'age': 2, breed : 'King Charles'},
+//   { 'name': 'prudence', 'age': 5, breed : 'Poodle'}
+// ];
 
-const helloWorld = function () {
-    console.log('Hello World!');
+// _.findWhere(dogs, { 'breed': 'King Charles' }); // snickers object
+
+// module.exports = {
+//   output: lodash.without([1, 2, 3], 1)
+// }
+
+
+export function output(){
+  console.log(lodash.without([1, 2, 3], 1));
+};
+
+export function helloWorld(){
+  console.log('Hello World!');
+};
+
+export class Person{
+  constructor(name, surname){
+    this.name = name;
+    this.surname = surname;
   }
 
-
-const randomNumber = function () {
-    console.log(uniqueRandomArray([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]));
+  whoAmI(){
+    console.log("I am "+ this.name + " "+ this.surname);
   }
-
-
-module.exports = {
-    
-    helloWorld,
-    randomNumber
 }
